cb554673ffa7f54bcdc483cfafee9e23701ba2f6
mata <mata@matas-MacBook-Air.local>
2021-11-15 20:52:56 +0100

    add matching functionality and chat real time

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import Header from '../components/Header'
+import ChatList from '../components/ChatList'
 
 const ChatScreen = () => {
     return (
-        <View>
-            <Text>I am the tax screen</Text>
-        </View>
+        <SafeAreaView>
+            <Header title="Chat" />
+            <ChatList />
+        </SafeAreaView>
     )
 }
 
